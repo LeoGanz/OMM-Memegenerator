1b32ce02d0464c24a4b621faf07615eaf4d53b3a
PhilAdam <phil.crom@outlook.de>
2021-12-04 13:25:02 +0100

    Maybe resolved promise but idk

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,12 +1,15 @@
+import {connect} from "mongoose";
+
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 import User from '../models/user.js'
 
 router.get('/register', (req, res, next) => {
-    var promise = new Promise(function (resolve, reject) {
 
-        const mongoDB = 'mongodb://localhost:27017/user';
+    var connected;
+    const mongoDB = 'mongodb://localhost:27017/user';
+    var promise = new Promise(function (resolve, reject) {
         mongoose.connect(mongoDB);
         const db = mongoose.connection;
         db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -23,7 +26,6 @@ router.get('/register', (req, res, next) => {
                 const jwt = require('njwt');
                 const claims = {username: username};
                 const token = jwt.create(claims, 'top-sectret');
-                token.setExpiration();
                 const jwtTokenString = token.compact();
 
                 const user = {      //TODO: check how credentials are send over with the request
@@ -35,17 +37,27 @@ router.get('/register', (req, res, next) => {
                     lastEdited: [],
                     lastComments: []
                 };
+
                 User.create(user).then(doc => {
-                    next();
-                }).catch(err => { //TODO: check for any suitable error handling
+                    connected = true;
+                }).catch(err => {
+                    connected = false; //TODO: check for any suitable error handling
                 });
             } else {
                 //TODO: Consider what to do, when the registration token is already in the db
             }
         })
     })
+    promise.then(function (value) {
+        if (connected) {
+            next();
+        } else {
+            res.set('WWW-Authenticate', 'Basic realm="401"')
+            res.status(401).send()
+            return;
+        }
+    })
 });
-
 
 
 module.exports = router;

e03883f480c963abd5d17df68534252bba8f88cb
PhilAdam <phil.crom@outlook.de>
2021-12-09 10:26:38 +0100

    finalized eternal.js Now trying to do an utils file

--- server/routes/eternal.js
+++ server/routes/eternal.js
@@ -25,9 +25,24 @@ router.get('/', (req, res, next) => {
     if (checkForToken(token)) {
         next();
     } else if (credentials !== undefined) {
-        mongoose.connect(mongoDB).then(()=> {
+        mongoose.connect(mongoDB).then(() => {
+            const db = mongoose.connection;
+            db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+            console.log('db connection initiated');
+            let namePW = credentials.split(" ");
+            let name = namePW[0];
+            let pw = namePW[1];
+            db.collection("users").findOne({username: name, password: pw}, (err) => {
+                if(err){
+                    console.log("401: Wrong user-credentials given");
+                    res.status(401).send("Wrong user-credentials given");
+                    return;
+                }else{
+                    next();
+                }
+            });
 
-        }).catch(()=>{
+        }).catch(() => {
             console.log("503: Connection do db failed");
             res.status(503).send("Connection do db failed");
             return;

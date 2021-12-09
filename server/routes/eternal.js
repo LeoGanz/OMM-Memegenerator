2045bf415db830a79337d3c6b11fd68bb1d7a776
PhilAdam <phil.crom@outlook.de>
2021-12-09 10:23:07 +0100

    Removed the user from the mongoDB variable

--- server/routes/eternal.js
+++ server/routes/eternal.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const jwt = require("njwt");
 const router = express.Router();
-const mongoDB = 'mongodb://localhost:27017/user';
+const mongoDB = 'mongodb://localhost:27017';
 
 function checkForToken(token) {
     let isThere = false;
@@ -25,10 +25,17 @@ router.get('/', (req, res, next) => {
     if (checkForToken(token)) {
         next();
     } else if (credentials !== undefined) {
+        mongoose.connect(mongoDB).then(()=> {
 
+        }).catch(()=>{
+            console.log("503: Connection do db failed");
+            res.status(503).send("Connection do db failed");
+            return;
+        });
     } else {
-
+        console.log("401: No authorization to do this");
         res.status(401).send("No authorization to do this");
+        return;
     }
 });
 

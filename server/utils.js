6564d26efb59d1731f526cef4537fbfd14de737c
PhilAMobil <philip.adamczyk@freenet.de>
2022-01-10 16:29:10 +0100

    deleted check in DB

--- server/utils.js
+++ server/utils.js
@@ -1,38 +1,10 @@
-const jwt = require("njwt");
-
 module.exports = function () {
-    this.checkForToken = function (token) {
-        let isThere = false;
-        jwt.verify(token, "top-secret", (err, verifiedJwt) => {
-            console.log(verifiedJwt);
-            console.log(token);
-            if (err) {
-                console.log(err);
-            } else {
-                isThere = true;
-                console.log(verifiedJwt);
-            }
-        });
-        return isThere;
-    };
 
     this.giveBackDateString = function () {
         const today = new Date();
         const creationDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
             + '--' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
         return creationDate;
-    }
-
-    this.checkInDB = function (schema, part) {
-        let lst;
-        schema.find(part, (err, l) => {
-            if (err) {
-                lst = [];
-            } else {
-                lst = l;
-            }
-        });
-        return lst;
     }
 
     this.findUpdateSingleValueDB = function (schema, partFind, partUpdate) {

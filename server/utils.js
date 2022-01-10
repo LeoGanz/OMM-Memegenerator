c6bb06ae008340592f44aa10e38a05265d000a58
PhilAMobil <philip.adamczyk@freenet.de>
2022-01-10 16:53:15 +0100

    adjusted the utils file completely

--- server/utils.js
+++ server/utils.js
@@ -6,16 +6,6 @@ module.exports = function () {
             + '--' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
         return creationDate;
     }
-
-    this.findUpdateSingleValueDB = function (schema, partFind, partUpdate) {
-        schema.findOneAndUpdate(partFind, partUpdate, (err) => {
-            if (err) {
-                return false;
-            } else {
-                return true;
-            }
-        });
-    }
 }
 
 

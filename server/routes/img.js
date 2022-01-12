f07ecfb5b65b1121d88046f908270e48e79623be
PhilAdam <phil.crom@outlook.de>
2022-01-12 13:51:46 +0100

    updated img.js to be hopefully right

--- server/routes/img.js
+++ server/routes/img.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const router = express.Router();
-const mongoDB = 'mongodb://localhost:27017/images';
+const mongoDBImages = 'mongodb://localhost:27017/images';
+const mongoDBUsers = 'mongodb://localhost:27017/users';
 const pictureSchema = require("../models/pictureSchema");
+const userSchema = require("../models/userSchema");
 const utils = require("../utils");
 const ut = new utils();
 const multer = require('multer');
@@ -21,40 +23,71 @@ const upload = multer({storage: storage});
 
 
 router.get("/", (req, res) => {
-    mongoose.connect(mongoDB).then(() => {
+    mongoose.connect(mongoDBImages).then(() => {
         mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
         console.log('db connection initiated');
         pictureSchema.find({}, (err, items) => {
             if (err) {
                 console.log(err);
                 res.status(500).send('Image not findable' + err);
-            }
-            else {
-                res.render('imagesPage', { items: items });
+            } else {
+                res.render('imagesPage', {items: items});
             }
         });
     });
 })
 
 router.post('/', upload.single('image'), (req, res) => {
-    const uploads_dir = path.join(__dirname + '/uploads/' + req.file.filename);
-    console.log(uploads_dir);
-    const img = {
-        name: req.body.name,
-        desc: req.body.desc,
-        img: {
-            data: fs.readFileSync(uploads_dir),
-            contentType: 'image/png'
-        }
-    };
-    pictureSchema.create(img, (err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            item.save();
-            res.redirect('/');
-        }
+    mongoose.connect(mongoDBUsers).then(() => {
+        mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        console.log('db connection initiated');
+        userSchema.find({currentToken: req.query.token}, (err, lst) => {
+            if (err) {
+                console.log("503: Connection to db failed");
+                res.status(503).send("Connection to db failed");
+            } else {
+                if(lst.length === 0){
+                    console.log("503: No user with this token found");
+                    res.status(503).send("No user with this token found");
+                }
+                const createUser = lst[0];
+                mongoose.connect(mongoDBImages).then(() => {
+                    mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+                    console.log('db connection initiated');
+                    const uploads_dir = path.join(__dirname + '/uploads/' + req.file.filename);
+                    console.log(uploads_dir);
+                    const dateString = ut.giveBackDateString();
+                    const img = {
+                        name: req.body.name,
+                        desc: req.body.desc,
+                        img: {
+                            data: fs.readFileSync(uploads_dir),
+                            contentType: 'image/png'
+                        },
+                        creator: {createUser},
+                        dateOfCreation: dateString,
+                        upVoters: [],
+                        downVoters: [],
+                        comments: [],
+                        metadata: req.body.metadata,
+                        format: {
+                            width: req.body.width,
+                            height: req.body.height,
+                            pixels: req.body.pixels
+                        }
+                    };
+
+                    pictureSchema.create(img, (err, item) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            item.save();
+                            res.redirect('/');
+                        }
+                    });
+                });
+            }
+        });
     });
 });
 

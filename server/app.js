0910ee85248c6c69964437069b193b9061c61bb9
PhilAdam <phil.crom@outlook.de>
2022-01-12 13:17:57 +0100

    added templates from site to img.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use('/', imgRouter); //showing images
+app.use('/images', imgRouter); //showing images
 
 app.use('/users', usersRouter); //displaying all users
 app.use('/register', registerRouter); //register-activity
--- server/routes/img.js
+++ server/routes/img.js
@@ -6,6 +6,7 @@ const imgSchema = require("../models/imageSchema");
 const utils = require("../utils");
 const ut = new utils();
 const multer = require('multer');
+const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -19,8 +20,40 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 
-router.get("",(req,res)=>{
-
+router.get("/", (req, res) => {
+    mongoose.connect(mongoDB).then(() => {
+        mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        console.log('db connection initiated');
+        imgSchema.find({}, (err, items) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Image not findable' + err);
+            }
+            else {
+                res.render('imagesPage', { items: items });
+            }
+        });
+    });
 })
+
+router.post('/', upload.single('image'), (req, res, next) => {
+    const img = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    };
+    imgSchema.create(img, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            item.save();
+            res.redirect('/');
+        }
+    });
+});
 
 module.exports = router;

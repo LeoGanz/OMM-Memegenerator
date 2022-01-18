0629f5d7e7809a3d7b18e8d95a70e84ac30e1597
PhilAdam <phil.crom@outlook.de>
2022-01-18 11:35:41 +0100

    everything pushing should be fine in img

--- server/routes/img.js
+++ server/routes/img.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pictureSchema = require("../models/pictureSchema");
-// const userSchema = require("../models/userSchema");
+const commentSchema = require("../models/commentSchema");
 const utils = require("../utils");
 const userSchema = require("../models/userSchema");
 const ut = new utils();
@@ -20,23 +20,102 @@ const ut = new utils();
 
 // const upload = multer({storage: storage});
 
-function handleUp(metadata, userToken, res) {
-    userSchema.find({currentToken: userToken}, (err, lst) => {
+function handleUp(metadata, user, res) {
+    pictureSchema.find({metadata: metadata}, (err, lst) => {
         if (err) {
-            console.log("503: Connection to db failed");
-            res.status(503).send("Connection to db failed");
+            console.log("503: Connection to db picture failed");
+            res.status(503).send("Connection to db picture failed");
         } else {
             if (lst.length === 0) {
-                console.log("503: No user with this token found");
-                res.status(503).send("No user with this token found");
+                console.log("400: No picture with this metadata found");
+                res.status(400).send("No picture with this metadata found");
             }
-            const createUser = lst[0];
+            let pict = lst[0];
+            let upVoters = pict.upVoters;
+            if (user in upVoters) {
+                console.log("400: You have already up voted this");
+                res.status(400).send("You have already up voted this");
+            }
+            pict.downVoters = pict.downVoters.filter((elem) => elem !== user);
+            pict.upVoters.push(user);
 
+            console.log("200: Picture update succeeded");
+            res.status(200).send("Picture update succeeded");
         }
     });
 }
 
-function handleDown(metadata, userToken, res) {
+function handleDown(metadata, user, res) {
+    pictureSchema.find({metadata: metadata}, (err, lst) => {
+        if (err) {
+            console.log("503: Connection to db picture failed");
+            res.status(503).send("Connection to db picture failed");
+        } else {
+            if (lst.length === 0) {
+                console.log("400: No picture with this metadata found");
+                res.status(400).send("No picture with this metadata found");
+            }
+            let pict = lst[0];
+            let downVoters = pict.downVoters;
+            if (user in downVoters) {
+                console.log("400: You have already down voted this");
+                res.status(400).send("You have already down voted this");
+            }
+            pict.upVoters = pict.upVoters.filter((elem) => elem !== user);
+            pict.downVoters.push(user);
+
+            console.log("200: Picture downdate succeeded");
+            res.status(200).send("Picture downdate succeeded");
+        }
+    });
+}
+
+function handleComment(comment, metadata, user, res) {
+    const currentDate = ut.giveBackDateString();
+    const comm = {
+        dateOfCreation: currentDate,
+        creator: user,
+        comment: comment
+    };
+    pictureSchema.find({metadata: metadata}, (err, lst) => {
+        if (err) {
+            console.log("503: Connection to db picture failed");
+            res.status(503).send("Connection to db picture failed");
+        } else {
+            if (lst.length === 0) {
+                console.log("400: No picture with this metadata found");
+                res.status(400).send("No picture with this metadata found");
+            }
+            let pict = lst[0];
+            commentSchema.create(comm, err => {
+                console.log("503: Connection to db comment failed");
+                res.status(503).send("Connection to db comment failed");
+
+                commentSchema.find({comment: comment, creator: user}, (err, lst) => {
+                    if(err){
+                        console.log("503: Connection to db comment failed");
+                        res.status(503).send("Connection to db comment failed");
+                    }else{
+                        if (lst.length === 0) {
+                            console.log("400: No comment with this user and string found");
+                            res.status(400).send("No comment with this user and string found");
+                        }
+                        pict.comments.push(lst[0]);
+                        console.log("200: Picture comment add succeeded");
+                        res.status(200).send("Picture comment add succeeded");
+                    }
+                });
+            });
+        }
+    });
+}
+
+router.post("/", (req, res) => {
+    let metadata = req.body.metadata;
+    let userToken = req.body.token;
+    let upVote = req.body.up;
+    let downVote = req.body.down;
+    let comment = req.body.comment;
     userSchema.find({currentToken: userToken}, (err, lst) => {
         if (err) {
             console.log("503: Connection to db user failed");
@@ -46,46 +125,18 @@ function handleDown(metadata, userToken, res) {
                 console.log("400: No user with this token found");
                 res.status(400).send("No user with this token found");
             }
-            const createUser = lst[0];
-            pictureSchema.find({metadata:metadata},(err, lst) =>{
-                if (err) {
-                    console.log("503: Connection to db picture failed");
-                    res.status(503).send("Connection to db picture failed");
-                } else {
-                    if (lst.length === 0) {
-                        console.log("400: No picture with this metadata found");
-                        res.status(400).send("No picture with this metadata found");
-                    }
-                    let pict = lst[0];
-                    let upvoters = pict.upVoters;
-                    if (createUser in upvoters){
-                        console.log()
-                    }
-                }
-            });
+            let user = lst[0];
+            if (upVote !== undefined) {
+                handleUp(metadata, user, res);
+            }
+            if (downVote !== undefined) {
+                handleDown(metadata, user, res);
+            }
+            if (comment !== undefined) {
+                handleComment(comment, metadata, user, res);
+            }
         }
     });
-}
-
-function handleComment(comment, metadata, userToken, re) {
-
-}
-
-router.post("/", (req, res) => {
-    let metadata = req.body.metadata;
-    let userToken = req.body.token;
-    let upVote = req.body.up;
-    let downVote = req.body.down;
-    let comment = req.body.comment;
-    if (upVote !== undefined) {
-        handleUp(metadata, userToken, res);
-    }
-    if (downVote !== undefined) {
-        handleDown(metadata, userToken, res);
-    }
-    if (comment !== undefined) {
-        handleComment(comment, metadata, userToken, res);
-    }
 });
 
 router.get("/", (req, res) => {

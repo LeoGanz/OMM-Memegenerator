fb6cca12f1d45a99e5056a76b1037a32fabbda77
PhilAdam <phil.crom@outlook.de>
2022-01-22 12:14:55 +0100

    added an undefined check for every elem

--- server/routes/retrieve.js
+++ server/routes/retrieve.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
     const {numberOfMemes} = req.query;
     const text = req.query.text;
     const {creatorName} = req.query;
-    const {creationDate} = req.query;
+    let {creationDate} = req.query;
 
     textSchema.find({}, (err, lst) => {
         if (err) {
@@ -17,13 +17,17 @@ router.get('/', (req, res) => {
             res.status(503).send("Connection to db texts failed");
         } else {
             let possibleTexts = [];
-            if (lst.length === 0) {
+            if (lst.length === 0 && text !== undefined) {
                 console.log("200: No meme-texts found");
                 res.status(200).send("No meme-texts found");
             } else {
-                possibleTexts = lst.filter((elem) => {
-                    return elem.text.contains(text);
-                });
+                if (lst.length !== 0) {
+                    possibleTexts = lst.filter((elem) => {
+                        return elem.text.contains(text);
+                    });
+                } else {
+                    possibleTexts = "";
+                }
             }
             userSchema.find({}, (err, lst) => {
                 if (err) {
@@ -37,11 +41,14 @@ router.get('/', (req, res) => {
                         let possibleUsers = lst.filter((elem) => {
                             return elem.username === creatorName;
                         });
-                        if (possibleUsers.length === 0) {
+                        if (possibleUsers.length === 0 && creatorName !== undefined) {
                             console.log("400: No user with this name found");
                             res.status(400).send("No user with this name found");
                         }
-                        let creator = possibleUsers[0];
+                        let creator = "";
+                        if (possibleUsers.length !== 0) {
+                            creator = possibleUsers[0];
+                        }
                         pictureSchema.find({}, (err, lst) => {
                             if (err) {
                                 console.log("503: Connection to db pictures failed");
@@ -52,6 +59,15 @@ router.get('/', (req, res) => {
                                     res.status(200).send("No pictures in the db");
                                 } else {
                                     let possiblePictures = lst.filter((elem) => {
+                                        if (creationDate === undefined) {
+                                            creationDate = elem.dateOfCreation;
+                                        }
+                                        if (creator === "") {
+                                            creator = elem.creator;
+                                        }
+                                        if (possibleTexts === ""){
+                                            possibleTexts = elem.texts;
+                                        }
                                         return (elem.dateOfCreation.contains(creationDate)
                                             && elem.creator === creator
                                             && [...new Set([...possibleTexts, ...elem.texts])] === possibleTexts);
@@ -60,7 +76,12 @@ router.get('/', (req, res) => {
                                         console.log("200: No memes with these parameters found");
                                         res.status(200).send("No memes with these parameters found");
                                     } else {
-                                        let slicedList = possiblePictures.slice(0, numberOfMemes);
+                                        let slicedList;
+                                        if (numberOfMemes !== undefined) {
+                                            slicedList = possiblePictures.slice(0, numberOfMemes);
+                                        } else {
+                                            slicedList = possiblePictures;
+                                        }
                                         for (let meme in slicedList) {
                                             result = result + "localhost:3000/image?metadata=" + meme.metadata + "\n";
                                         }

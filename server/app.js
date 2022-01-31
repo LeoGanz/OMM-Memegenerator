9dba4059fc80c039f4d2167dadeb5b0e086ac126
Leonard Ganz <leonard@ruediger-ganz.de>
2022-01-31 20:34:35 +0100

    Fix upload of large files by increasing limits to 50mb

--- server/app.js
+++ server/app.js
@@ -27,8 +27,8 @@ let app = express();
 
 const corsOptions = {origin: true, credentials: true}
 app.options('*', cors(corsOptions));
-app.listen(80, function(){
-    console.log('CORS-enabled web server listening on port 80');
+app.listen(8000, function(){
+    console.log('CORS-enabled web server listening on port 8000');
 });
 
 mongoose.connect(process.env.MONGO_URL,
@@ -46,8 +46,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false}));
 app.use(cookieParser());
 
 //Image Storage

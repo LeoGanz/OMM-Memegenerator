7ba207d51963c2b7abcf8712b2f8485e864cc36e
PhilAdam <phil.crom@outlook.de>
2022-02-05 17:56:55 +0100

    example working

--- client/src/pages/template-graph.tsx
+++ client/src/pages/template-graph.tsx
@@ -1,7 +1,98 @@
-export const TemplateGraph = ()=>{
+import React, {PureComponent} from 'react';
+import {useContext, useState} from "react";
+import LoginContext from "../login-context";
+import {useNavigate} from "react-router-dom";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer
+} from "recharts";
+
+const ExampleData = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    }
+]
+
+export const TemplateGraph = () => {
+    const {isLoggedIn} = useContext(LoginContext)
+    let navigate = useNavigate()
+    let jwt = "";
+    const [memeIds, setMemeIds] = useState<string[]>([]);
+    const [usages, setUsages] = useState<number[]>([]);
+    if (isLoggedIn) {
+        jwt = localStorage.getItem('meme-token') || "";
+    }
     return (
         <>
-
+            <LineChart
+                width={500}
+                height={300}
+                data={ExampleData}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                    type="monotone"
+                    dataKey="pv"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
         </>
     )
 }

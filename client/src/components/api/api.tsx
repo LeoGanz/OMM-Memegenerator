8b9b982d758cff2e20fe079490bb9693cda05127
PhilAdam <phil.crom@outlook.de>
2022-02-05 12:02:19 +0100

    Documentation text working without a Link. Next is to input the link

---
+++ client/src/components/api/api.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const StyledLink = styled("link")`
+  color: palevioletred;
+  font-weight: bold;
+`
--- client/src/pages/api-doc.tsx
+++ client/src/pages/api-doc.tsx
@@ -1,12 +1,40 @@
 import {Title} from "../components/layout/typography";
+import {StyledLink} from "../components/api/api";
 
+
+const retrieveText = {}
 
 export const APIDoc = () => {
-    return(
+    return (
         <>
             <Title>
                 Documentation - API
             </Title>
+            <p>
+                For retrieving memes a machine needs to make a GET-request. There need to be certain
+                parameters given
+                within the query. An essential parameter needed to be
+                given is the number of memes to retreive as Integer-value. Sending every meme is too
+                much for the API, so this is needed.
+                The optional parameters are for searching memes. You can search for texts in memes.
+                Therefore give a String to the
+                query-parameter "text". Another parameter is the username of the creator. Therefore
+                give a String to the query parameter
+                "username". The last possible parameter is the creation date of the meme in
+                String-format needed to be given to the parameter "creationDate".
+                The format is JJJJ-MM-TT, but leave out the leading zeros. So the first of February
+                123 looks like this: 123-2-1.
+
+                An example request would look like this:
+            </p>
+            <p/>
+            <p/>
+            <p/>
+            <p>
+
+                If you click on the on the URL. You get to a new page giving back URLs to every meme
+                found for these parameters given.
+            </p>
         </>
     )
 }

1847e2d260c6e2fbdc439b2d266a873511fd0a06
PhilAdam <phil.crom@outlook.de>
2022-02-09 10:07:34 +0100

    profile.js corrected

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,7 +1,34 @@
 let express = require('express');
 let router = express.Router();
 const userSchema = require("../models/userSchema");
-const {dbConnectionFailureHandler, respond} = require("../utils");
+const memeSchema = require("../models/memeSchema");
+const {dbConnectionFailureHandler, respond, respondSilently} = require("../utils");
+
+
+function findMemeBasesAndRespond(comments, memeComments, res, response) {
+    if (comments.length === 0) {
+        response.comments = memeComments;
+        respond(res, 200, response);
+    } else {
+        let comm = comments.shift();
+        console.log(comm.memeId);
+        memeSchema.find({memeId: comm.memeId}, (err, lst) => {
+            if (err) {
+                respondSilently(res, 500, "Connection to memeDatabase failed");
+            } else {
+                console.log(lst[0]);
+                const base = lst[0].img.base64;
+                const newComm = {
+                    dateOfCreation: comm.dateOfCreation,
+                    text: comm.text,
+                    base64: base
+                }
+                memeComments.push(newComm);
+                findMemeBasesAndRespond(comments,memeComments, res, response);
+            }
+        });
+    }
+}
 
 
 /**
@@ -19,28 +46,22 @@ router.get('/', (req, res) => {
                 respond(res, 400, "Impossible Error: No user with this token found");
             } else {
                 const user = lst[0];
-                console.log("User found:");
-                if (err) {
-                    respond(res, 500, 'No user found');
-                } else {
-                    const username = user.username;
-                    const fullName = user.fullName;
-                    const email = user.email;
-                    let history = user.lastEdited;
-                    let comments = user.lastComments;
-                    const start = req.query.start ?? 0;
-                    const end = req.query.end ?? 1;
-                    history = history.slice(start, end);
-                    comments = comments.slice(start,end);
-                    let response = {
-                        username: username,
-                        fullName: fullName,
-                        email: email,
-                        memeHistory: history,
-                        comments: comments
-                    }
-                    respond(res, 200, response);
+                const username = user.username;
+                const fullName = user.fullName;
+                const email = user.email;
+                let history = user.lastEdited;
+                let comments = user.lastComments;
+                const start = req.query.start ?? 0;
+                const end = req.query.end ?? 5;
+                history = history.slice(start, end);
+                comments = comments.slice(start, end);
+                let response = {
+                    username: username,
+                    fullName: fullName,
+                    email: email,
+                    memeHistory: history,
                 }
+                findMemeBasesAndRespond(comments, [], res, response);
             }
         }
     });

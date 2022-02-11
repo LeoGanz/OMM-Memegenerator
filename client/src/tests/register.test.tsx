99a9e5b60b5124e0b0a3e000fbf29f8abd55469b
PhilAdam <phil.crom@outlook.de>
2022-02-11 12:38:26 +0100

    register.test.tsx working

---
+++ client/src/tests/register.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'jest-styled-components';
+import {SignUp} from '../pages/sign-up';
+import {render} from '@testing-library/react';
+
+
+let mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom') as any,
+    useNavigate: () => mockedNavigate,
+}));
+
+test('Displayed properly except Submit', () => {
+    const {getByText, getAllByText} = render(<SignUp/>);
+    const title = getByText("Sign Up");
+    expect(title).toBeDefined();
+    const textInputs = getAllByText("");
+    expect(textInputs[0]).toBeDefined();
+    expect(textInputs[1]).toBeDefined();
+    expect(textInputs[2]).toBeDefined();
+    expect(textInputs[3]).toBeDefined();
+});
